476c67628281abc27d2ae176eb80fe88c36c1b68
Nordine Bittich <nordine@bittich.be>
2023-09-30 23:05:32 +0200

    autocommit

--- src/adana_script/tests/bitwise.rs
+++ src/adana_script/tests/bitwise.rs
@@ -8,6 +8,8 @@ use crate::adana_script::compute;
 fn bitwise_or_test() {
     let mut ctx = BTreeMap::new();
 
+    let r = compute(r#"1|2"#, &mut ctx, "N/A").unwrap();
+    assert_eq!(r, Primitive::U8(3));
     let r = compute(r#"1|1"#, &mut ctx, "N/A").unwrap();
     assert_eq!(r, Primitive::U8(1));
     let r = compute(r#"1|0"#, &mut ctx, "N/A").unwrap();
@@ -21,5 +23,27 @@ fn bitwise_or_test() {
     let r = compute(r#"-1|1"#, &mut ctx, "N/A").unwrap();
     assert_eq!(r, Primitive::I8(-1));
     let r = compute(r#"-98|1"#, &mut ctx, "N/A").unwrap();
+    assert_eq!(r, Primitive::I8(-97));
+}
+
+#[test]
+fn bitwise_xor_test() {
+    let mut ctx = BTreeMap::new();
+
+    let r = compute(r#"1$2"#, &mut ctx, "N/A").unwrap();
+    assert_eq!(r, Primitive::U8(3));
+    let r = compute(r#"1$1"#, &mut ctx, "N/A").unwrap();
+    assert_eq!(r, Primitive::U8(0));
+    let r = compute(r#"1$0"#, &mut ctx, "N/A").unwrap();
+    assert_eq!(r, Primitive::U8(1));
+    let r = compute(r#"0$0"#, &mut ctx, "N/A").unwrap();
+    assert_eq!(r, Primitive::U8(0));
+    let r = compute(r#"127$135"#, &mut ctx, "N/A").unwrap();
+    assert_eq!(r, Primitive::U8(248));
+    let r = compute(r#"127$9"#, &mut ctx, "N/A").unwrap();
+    assert_eq!(r, Primitive::U8(118));
+    let r = compute(r#"-1$1"#, &mut ctx, "N/A").unwrap();
+    assert_eq!(r, Primitive::I8(-2));
+    let r = compute(r#"-98$1"#, &mut ctx, "N/A").unwrap();
     assert_eq!(r, Primitive::I8(-97));
 }
