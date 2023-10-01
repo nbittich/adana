2f27213754f03b6d6821fa794cdfa785e1c478fd
Nordine Bittich <nordine@bittich.be>
2023-10-01 11:14:50 +0200

    autocommit

--- src/adana_script/tests/bitwise.rs
+++ src/adana_script/tests/bitwise.rs
@@ -21,9 +21,9 @@ fn bitwise_or_test() {
     let r = compute(r#"127|9"#, &mut ctx, "N/A").unwrap();
     assert_eq!(r, Primitive::U8(127));
     let r = compute(r#"-1|1"#, &mut ctx, "N/A").unwrap();
-    assert_eq!(r, Primitive::I8(-1));
+    assert_eq!(r, Primitive::Int(-1));
     let r = compute(r#"-98|1"#, &mut ctx, "N/A").unwrap();
-    assert_eq!(r, Primitive::I8(-97));
+    assert_eq!(r, Primitive::Int(-97));
 }
 
 #[test]
@@ -43,9 +43,9 @@ fn bitwise_xor_test() {
     let r = compute(r#"127$9"#, &mut ctx, "N/A").unwrap();
     assert_eq!(r, Primitive::U8(118));
     let r = compute(r#"-1$1"#, &mut ctx, "N/A").unwrap();
-    assert_eq!(r, Primitive::I8(-2));
+    assert_eq!(r, Primitive::Int(-2));
     let r = compute(r#"-98$1"#, &mut ctx, "N/A").unwrap();
-    assert_eq!(r, Primitive::I8(-97));
+    assert_eq!(r, Primitive::Int(-97));
 }
 
 #[test]
@@ -59,10 +59,81 @@ fn bitwise_not() {
     let r = compute(r#"~128"#, &mut ctx, "N/A").unwrap();
     assert_eq!(r, Primitive::Int(-129));
 }
+
+#[test]
+fn bitwise_and() {
+    let mut ctx = BTreeMap::new();
+
+    let r = compute(r#"1@2"#, &mut ctx, "N/A").unwrap();
+    assert_eq!(r, Primitive::U8(1 & 2));
+    let r = compute(r#"1@1"#, &mut ctx, "N/A").unwrap();
+    assert_eq!(r, Primitive::U8(1 & 1));
+    let r = compute(r#"1@0"#, &mut ctx, "N/A").unwrap();
+    assert_eq!(r, Primitive::U8(1 & 0));
+    let r = compute(r#"0@0"#, &mut ctx, "N/A").unwrap();
+    assert_eq!(r, Primitive::U8(0 & 0));
+    let r = compute(r#"127@135"#, &mut ctx, "N/A").unwrap();
+    assert_eq!(r, Primitive::U8(127 & 135));
+    let r = compute(r#"127@9"#, &mut ctx, "N/A").unwrap();
+    assert_eq!(r, Primitive::U8(127 & 9));
+    let r = compute(r#"-1@1"#, &mut ctx, "N/A").unwrap();
+    assert_eq!(r, Primitive::Int(-1 & 1));
+    let r = compute(r#"-98@1"#, &mut ctx, "N/A").unwrap();
+    assert_eq!(r, Primitive::Int(-98 & 1));
+}
+
+#[test]
+fn test_left_shift() {
+    let mut ctx = BTreeMap::new();
+
+    let r = compute(r#"1<<2"#, &mut ctx, "N/A").unwrap();
+    assert_eq!(r, Primitive::Int(1 << 2));
+    let r = compute(r#"1 <<1"#, &mut ctx, "N/A").unwrap();
+    assert_eq!(r, Primitive::Int(1 << 1));
+    let r = compute(r#"1<< 0"#, &mut ctx, "N/A").unwrap();
+    assert_eq!(r, Primitive::Int(1 << 0));
+    let r = compute(r#"0 << 0"#, &mut ctx, "N/A").unwrap();
+    assert_eq!(r, Primitive::Int(0 << 0));
+    // FIXME probably need to use a library for this
+    // let r = compute(r#"127 <<135"#, &mut ctx, "N/A").unwrap();
+    // assert_eq!(r, Primitive::Int((127u64 << 135u64) as i128));
+    let r = compute(r#"127<<9"#, &mut ctx, "N/A").unwrap();
+    assert_eq!(r, Primitive::Int(127 << 9));
+    let r = compute(r#"-1 << 1"#, &mut ctx, "N/A").unwrap();
+    assert_eq!(r, Primitive::Int(-1 << 1));
+    let r = compute(r#"-98 << 1"#, &mut ctx, "N/A").unwrap();
+    assert_eq!(r, Primitive::Int(-98 << 1));
+}
+
+#[test]
+fn test_right_shift() {
+    let mut ctx = BTreeMap::new();
+
+    let r = compute(r#"1>>2"#, &mut ctx, "N/A").unwrap();
+    assert_eq!(r, Primitive::Int(1 >> 2));
+    let r = compute(r#"1 >>1"#, &mut ctx, "N/A").unwrap();
+    assert_eq!(r, Primitive::Int(1 >> 1));
+    let r = compute(r#"1>> 0"#, &mut ctx, "N/A").unwrap();
+    assert_eq!(r, Primitive::Int(1 >> 0));
+    let r = compute(r#"0 >> 0"#, &mut ctx, "N/A").unwrap();
+    assert_eq!(r, Primitive::Int(0 >> 0));
+    // FIXME probably need to use a library for this
+    // let r = compute(r#"127 >>135"#, &mut ctx, "N/A").unwrap();
+    // assert_eq!(r, Primitive::Int((127u64 >> 135u64) as i128));
+    let r = compute(r#"127>>9"#, &mut ctx, "N/A").unwrap();
+    assert_eq!(r, Primitive::Int(127 >> 9));
+    let r = compute(r#"-1 >> 1"#, &mut ctx, "N/A").unwrap();
+    assert_eq!(r, Primitive::Int(-1 >> 1));
+    let r = compute(r#"-98 >> 1"#, &mut ctx, "N/A").unwrap();
+    assert_eq!(r, Primitive::Int(-98 >> 1));
+}
 #[test]
 fn test_complex_math_wise() {
     let mut ctx = BTreeMap::new();
 
     let r = compute(r#"30*9 @9 -5/~3"#, &mut ctx, "N/A").unwrap();
-    assert_eq!(r, Primitive::Int(10));
+    assert_eq!(r, Primitive::Int((30 * 9 & 9 - 5 / !3) as i128));
+
+    let r = compute(r#"30*9 @9 -5/~3 << 1"#, &mut ctx, "N/A").unwrap();
+    assert_eq!(r, Primitive::Int((30 * 9 & 9 - 5 / !3 << 1) as i128));
 }
