fdad2bc8649f398c08b3b6887a6aebf90c247ef6
Nordine Bittich <nordine@bittich.be>
2024-02-07 22:00:49 +0100

    publish

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-cargo release  --no-publish  --no-tag  --no-push  --dependent-version  upgrade --execute $1
+cargo release  --no-tag  --no-push  --dependent-version  upgrade --execute $1
 git tag $1
 git push origin $1
 git push
