8af161932a4cbcf7a428a819dec4718913784443
Nordine Bittich <nordine@bittich.be>
2024-02-11 21:39:56 +0100

    use latest adana std version

--- adana-script/src/require_dynamic_lib.rs
+++ adana-script/src/require_dynamic_lib.rs
@@ -1,6 +1,6 @@
 #[cfg(not(target_arch = "wasm32"))]
 const STD_DOWNLOAD_URI: &str =
-    "https://github.com/nbittich/adana-std/releases/download/0.17.3/adana-std.tar.gz";
+    "https://github.com/nbittich/adana-std/releases/download/0.17.0/adana-std.tar.gz";
 
 #[cfg(not(target_arch = "wasm32"))]
 use std::process::{Command, Stdio};
